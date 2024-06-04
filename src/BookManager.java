b9c7151f27353b49b1f22372ad53674ea354cad2
Gui Alquieri <88469484+Alquieri@users.noreply.github.com>
2024-06-04 19:46:21 -0300

    Changed the class Book and BookMangaer

--- src/BookManager.java
+++ src/BookManager.java
@@ -1,18 +1,22 @@
 import java.util.ArrayList;
 public class BookManager {
 
-public static ArrayList<Book> books = new ArrayList<Book>();
+public static ArrayList<Book> books;
 
 public static void addBook(Book b){
 
     books.add(b);
 }
 public static void removeBook(String title){
+    if (books.isEmpty()){
+        System.out.println("\nNo books found");
+    }
     for (int i = 0; i < books.size(); i++){
 
         if (books.get(i).getTitle().equals(title)){
             System.out.println("\n"+books.get(i).toString() + "\nAre you sure you want to remove it?(y/n)");
             String answer = System.console().readLine();
+            
             if (answer.equals("y")){
                 books.remove(i);
             }
